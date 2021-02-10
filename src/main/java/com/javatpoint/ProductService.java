fe156288c730c228dba02ef1455fb6f86cad60df
shrutikumar Dalvi <shrdalvi@cisco.com>
2021-02-10 14:23:38 +0530

    first commit

---
+++ src/main/java/com/javatpoint/ProductService.java
@@ -0,0 +1,34 @@
+package com.javatpoint;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ProductService implements IProductService {
+	
+	@Override
+	public Response findAll() {
+		
+		Response response = new Response();
+		
+		ArrayList<Product> products = new ArrayList<Product>();
+		products.add(new Product(100, "Mobile", "CLK98123", 9000.00, 6));
+		products.add(new Product(101, "Smart TV", "LGST09167", 60000.00, 3));
+		products.add(new Product(102, "Washing Machine", "38753BK9", 9000.00, 7));
+		products.add(new Product(103, "Laptop", "LHP29OCP", 24000.00, 1));
+		products.add(new Product(104, "Air Conditioner", "ACLG66721", 30000.00, 5));
+		products.add(new Product(105, "Refrigerator ", "12WP9087", 10000.00, 4));
+		response.setProducts(products);
+		try {
+			response.setMessage(InetAddress.getLocalHost().getHostAddress() +" --"+InetAddress.getLocalHost().getHostName());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return response;
+	}
+}
---
+++ src/main/java/com/javatpoint/Response.java
@@ -0,0 +1,28 @@
+package com.javatpoint;
+
+import java.util.List;
+
+public class Response {
+	
+	private String message;
+
+	private List<Product> products;
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
